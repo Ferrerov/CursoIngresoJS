7664f5c96eddf2849b84e4d2fd2bb56824645872
Ferrerov <valenferrero01@gmail.com>
2021-07-14 12:52:59 -0300

    Avances clase 5

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,42 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let precio = 15000;
+	let estacion;
+	let destino;
+	let precioFinal;
 
+	estacion = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (estacion){
+		case "Invierno":
+			if (destino == "Bariloche"){
+				precioFinal = precio + precio * .2 ;
+			}
+			else if (destino == "Cataratas" || destino == "Cordoba"){
+				precioFinal = precio - precio * .1;
+			}
+			else{
+				precioFinal = precio - precio * .2;
+			}
+		
+		case "Verano":
+			if (destino == "Bariloche"){
+				precioFinal = precio - precio * .2;
+			}
+			else if (destino == "Cataratas" || destino == "Cordoba"){
+				precioFinal = precio + precio * .1;
+			}
+			else{
+				precioFinal = precio + precio * .2;
+			}
+
+		case "Otoño":
+		case "Primavera":
+			if (destino != "Cordoba"){
+				precioFinal = precio + precio * .1;
+			}
+
+	}
+	
 }//FIN DE LA FUNCIÓN
