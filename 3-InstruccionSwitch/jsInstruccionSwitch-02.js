4d62b45705be4ce0b7704166dcf23399c35d20ae
Ferrerov <valenferrero01@gmail.com>
2021-07-14 09:12:28 -0300

    Inicio ejercicios Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,31 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
 
+	mes = document.getElementById("txtIdMes").value;
 
-
+	switch(mes){
+		case "Julio":
+			alert("Abrigate que hace frío");
+			break;
+		case "Agosto":
+		alert("Abrigate que hace frío");
+		break;
+		case "Septiembre":
+			alert("Ya pasamos el frío, ahora calor!!!");
+			break;
+		case "Octubre":
+			alert("Ya pasamos el frío, ahora calor!!!");
+			break;
+		case "Noviembre":
+			alert("Ya pasamos el frío, ahora calor!!!");
+			break;
+		case "Diciembre":
+			alert("Ya pasamos el frío, ahora calor!!!");
+			break;
+		default:
+			alert("Falta para el invierno");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
